43daab89b1bf371fec2bd920a1f9a5dbc2cd11a3
Nathan Dunfield <nathan@dunfield.info>
2018-04-30 17:09:43 -0500

    Updates to work with pip 10, PHC 2.4.52, prerelease Sage (off by default) and Ubunutu 18.04 (need to set SAGE_FAT_BINARY to "no").

--- sage/scripts/05_more_python_pkgs.sh
+++ sage/scripts/05_more_python_pkgs.sh
@@ -1,6 +1,7 @@
 # Some standard Python packages
-sage -pip install --upgrade pip setuptools virtualenv wheel
+sage -pip install --upgrade "pip>10" setuptools virtualenv wheel
+sage -pip install olefile # Needed by pillow but not pulled in automatically
 sage -pip install mercurial mysql-python sqlalchemy pycosat
 sage -pip install --upgrade --no-binary :all: \
-     --upgrade-strategy only-if-needed \
-     pandas matplotlib
+     --no-build-isolation \
+     pandas matplotlib jupyterlab
