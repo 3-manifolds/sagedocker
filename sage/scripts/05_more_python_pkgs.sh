aebff3b09dbe2bd7e3ff3ee63e31e571fa72dd95
Nathan Dunfield <nathan@dunfield.info>
2018-05-01 11:48:58 -0500

    Another tweak to deal with pip 10

--- sage/scripts/05_more_python_pkgs.sh
+++ sage/scripts/05_more_python_pkgs.sh
@@ -1,6 +1,8 @@
 # Some standard Python packages
 sage -pip install --upgrade "pip>10" setuptools virtualenv wheel
-sage -pip install olefile # Needed by pillow but not pulled in automatically
+# The following two packages should be pulled in automatically by
+# pillow and sphinx, respectively, but aren't for whatever reason.
+sage -pip install olefile sphinxcontrib-websupport
 sage -pip install mercurial mysql-python sqlalchemy pycosat
 sage -pip install --upgrade --no-binary :all: \
      --no-build-isolation \
