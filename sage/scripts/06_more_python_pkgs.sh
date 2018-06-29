86be2058d707f91de350814071140aa3eb95e55f
Nathan Dunfield <nathan@dunfield.info>
2018-06-29 09:10:43 -0500

    Making ineractive mpl ploting work in Jupyter*Lab*

--- sage/scripts/06_more_python_pkgs.sh
+++ sage/scripts/06_more_python_pkgs.sh
@@ -7,3 +7,10 @@ sage -pip install mercurial mysql-python sqlalchemy pycosat
 sage -pip install --upgrade --no-binary :all: \
      --no-build-isolation \
      pandas matplotlib jupyterlab
+# This next mess is to make interactive plotting work with jupyterlab
+# We get a bleeding edge ipympl to get rid of some DeprecationWarnings,
+# see https://github.com/matplotlib/jupyter-matplotlib/issues/51
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sage -pip install git+https://github.com/matplotlib/jupyter-matplotlib
+/sage/local/bin/jupyter-labextension install @jupyter-widgets/jupyterlab-manager
