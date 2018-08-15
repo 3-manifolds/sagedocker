a62bfd05685e6c5dd6dd4e30aecabdffe140809b
Nathan Dunfield <nathan@dunfield.info>
2018-08-15 15:13:37 -0500

    Sage 8.2 -> 8.3; PARI URL changed (again); JupyterLab to 0.33.x

--- sage/scripts/06_more_python_pkgs.sh
+++ sage/scripts/06_more_python_pkgs.sh
@@ -8,9 +8,7 @@ sage -pip install --upgrade --no-binary :all: \
      --no-build-isolation \
      pandas matplotlib jupyterlab
 # This next mess is to make interactive plotting work with jupyterlab
-# We get a bleeding edge ipympl to get rid of some DeprecationWarnings,
-# see https://github.com/matplotlib/jupyter-matplotlib/issues/51
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sage -pip install git+https://github.com/matplotlib/jupyter-matplotlib
+sage -pip install -U subprocess32 ipympl
 /sage/local/bin/jupyter-labextension install @jupyter-widgets/jupyterlab-manager
