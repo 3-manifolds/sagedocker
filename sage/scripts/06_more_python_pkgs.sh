e2cf65742332b613509d51577896d9f43755ef9d
Nathan Dunfield <nathan@dunfield.info>
2019-02-23 11:20:39 -0600

    Switched MySQL python package to something more modern

--- sage/scripts/06_more_python_pkgs.sh
+++ sage/scripts/06_more_python_pkgs.sh
@@ -3,7 +3,7 @@ sage -pip install --upgrade "pip>10" setuptools virtualenv wheel
 # The following two packages should be pulled in automatically by
 # pillow and sphinx, respectively, but aren't for whatever reason.
 sage -pip install olefile sphinxcontrib-websupport
-sage -pip install mercurial mysql-python sqlalchemy pycosat
+sage -pip install mercurial mysqlclient sqlalchemy pycosat
 sage -pip install --upgrade --no-binary :all: \
      --no-build-isolation \
      pandas matplotlib jupyterlab
